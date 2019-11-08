189ee2bb7f20ee42155eb2f0f2e3fc8a5209b0dd
Vu Hong Duong <duong.vuhong.uet@gmail.com>
2019-11-08 19:25:13 +0900

    optimize chip 8 emulator opcode

--- chip8.h
+++ chip8.h
@@ -3,49 +3,38 @@
 
 #include <stdint.h>
 
-#define INTERPRETER_SPACE 512
-#define CHIP8_MEMORY_SIZE 4096
+#define CHIP8_MEMORY_SIZE       4096
+#define CHIP8_INTERPRETER_SPACE 512
 
-#define V_REGISTERS 16
-#define STACK_LEVEL 16
-#define KEYPAD 16
+#define CHIP8_V_REG_NUMBER 16
 
-#ifdef SUPPORT_SCHIP_48
-#define XSCREEN_WIDTH 128
-#define XSCREEN_HEIGHT 64
-#define XSCREEN_PIXELS (XSCREEN_WIDTH * XSCREEN_HEIGHT)
+#define CHIP8_STACK_LEVEL 16
 
-#define RPL_USER_FLAGS 16
+#define CHIP8_KEY_NUMBER 16
 
-#endif
+#define CHIP8_SCREEN_WIDTH  64 /* in pixels */
+#define CHIP8_SCREEN_HEIGHT 32 /* in pixels */
+#define CHIP8_SCREEN_PIXELS (CHIP8_SCREEN_WIDTH * CHIP8_SCREEN_HEIGHT)
 
-#define SCREEN_WIDTH 64
-#define SCREEN_HEIGHT 32
-#define SCREEN_PIXELS (SCREEN_WIDTH * SCREEN_HEIGHT)
-
-struct chip8 {
+typedef struct {
 	uint16_t opcode;
 	uint8_t memory[CHIP8_MEMORY_SIZE];
-	uint8_t reg8[V_REGISTERS];
-	uint16_t reg16;
+	uint8_t V[CHIP8_V_REG_NUMBER];
+	uint16_t I;
 	uint16_t pc;
-	uint16_t stack[STACK_LEVEL];
+	uint16_t stack[CHIP8_STACK_LEVEL];
 	uint16_t sp;
-	uint8_t gfx[SCREEN_PIXELS];
+	uint8_t gfx[CHIP8_SCREEN_PIXELS];
 	uint8_t delay_timer;
 	uint8_t sound_timer;
-	uint8_t key[KEYPAD];
+	uint8_t key[CHIP8_KEY_NUMBER];
 	uint8_t compa;
-#ifdef SUPPORT_SCHIP_48
-	uint8_t xgfx[XSCREEN_PIXELS];
-	uint8_t rpl[RPL_USER_FLAGS];
-	uint8_t extend;
-#endif
 	uint8_t draw_flag;
-};
+} chip8_t;
 
-extern void chip8_init(struct chip8 *);
-extern int chip8_load(struct chip8 *, const char *);
-extern void chip8_emulate_cycle(struct chip8 *);
+extern void chip8_init(chip8_t *);
+extern int chip8_load(chip8_t *, const char *);
+extern void chip8_emulate_cycle(chip8_t *);
+extern void chip8_tick(chip8_t *);
 
-#endif
+#endif /* _CHIP8_H */
